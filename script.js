9ea09bcbb7e4beba09a7690f8d6b28f9f789ea78
thegreatmp <palmer.matthewj@gmail.com>
2021-12-28 12:57:16 +0900

    comp and player funtions, playRound switch, start score keeping

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function computerSelection (){
     const rps = ["rock", "paper", "scissors"];
 
@@ -13,8 +14,59 @@ function playerSelection () {
     return getPlayerInput.toLowerCase();
 }
 
-function getResult () {
-    computerSelection();
-    playerSelection ();
-    return `${computerSelection}` + `${playerSelection}`;
+let playRound = `${playerSelection()}${computerSelection()}`;
+
+switch (playRound) {
+    case 'rockrock':
+        console.log('Your Rock vs their Rock - neither are hard enough. Tie.');
+        break;
+    case 'rockpaper':
+        console.log('Your Rock is covered by their Paper. You LOSE.');
+        break;
+    case 'rockscissors':
+        console.log('Your Rock smashes their Scissors. You WIN.');
+        break;
+    case 'paperrock':
+        console.log('Your Paper covers their Rock. You WIN.');
+        break;
+    case 'paperpaper': 
+        console.log('A Paper-Paper slap fight. Tie.');
+        break;
+    case 'paperscissors':
+        console.log('Your Paper is cut by their Scissors. You LOSE.');
+        break;
+    case 'scissorsrock':
+        console.log('Your Scissors is smashed by their Rock. You LOSE.');
+        break;
+    case 'scissorsscissors':
+        console.log('Scissors on scissors crime is not a thing. Tie.');
+        break;
+    case 'scissorspaper':
+        console.log('Your Scissors cuts their paper. You WIN.');
+        break;
+    default:
+        console.log('Invalid choice, try again.');
 }
+
+
+let tie = 0;
+let computerScore = 0;
+let playerScore = 0;
+console.log(showGameScore());
+console.log(playRound);
+
+function showGameScore(){
+    return `You:${playerScore}   Them:${computerScore}   Nobody:${tie}`;
+}
+
+if (playRound = 'rockrock' || 'scissorsscissors' || 'paperpaper') {
+    showGameScore (playerScore, computerScore, ++tie);
+    console.log(showGameScore());
+} else if (playRound = 'rockscissors' || 'paperrock' || 'scissorspaper') {
+    showGameScore (++playerScore, computerScore, tie);
+} else if (playRound = 'rockpaper' || 'paperscissors' || 'scissorsrock'){
+    showGameScore (playerScore, ++computerScore, tie);
+}
+
+
+
